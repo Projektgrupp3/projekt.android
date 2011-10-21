08656f095eb4e562d736fd71b084d4acd65b77d3
David Buö <davidbauwie@gmail.com>
2011-10-21 10:14:03 +0200

    nu funkar det som det ska!

--- projekt.android/src/tddd36/grupp3/androidActivity.java
+++ projekt.android/src/tddd36/grupp3/androidActivity.java
@@ -1,8 +1,9 @@
-package Projekt;
+package tddd36.grupp3;
 
 import tddd36.grupp3.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class androidActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,5 +11,6 @@ public class androidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Log.d("Fel1","HŠr blev det fel");
     }
 }
