37a6bb1431cc9e05c75d81fcb91bbaf7d2c8ce90
Thomas Schytzler <thomas.schytzler@gmail.com>
2011-10-25 10:12:59 +0200

    Thomas Ändringar

--- projekt.android/src/tddd36/grupp3/androidActivity.java
+++ projekt.android/src/tddd36/grupp3/androidActivity.java
@@ -4,17 +4,48 @@ import tddd36.grupp3.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class androidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Log.d("Fel1","HŠr blev det fel");
-        Log.d("Fel1","HŠr blev det fel");
-        Log.d("Fel1","HŠr blev det fel");
-        Log.d("Fel1","HŠr blev det fel");
-        Log.d("Fel1","HŠr blev det rätt");
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+		Button login;
+		final TextView display;
+		final EditText user;
+		final EditText pass;
+		pass = (EditText) findViewById(R.id.editText2);
+		user = (EditText) findViewById(R.id.editText1);
+		login = (Button) findViewById(R.id.button1);
+		display = (TextView)findViewById(R.id.textView3);
+
+		login.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				display.setText("Loggar in...");
+				if (user.getText().length() == 0||pass.getText().length() == 0) {
+					display.setText("Du måste fylla i uppgifterna");
+				}
+				else{
+					display.setText("Loggar in...");
+
+				}
+
+
+
+			};
+		});
+
+
+
+
+	}
 }
