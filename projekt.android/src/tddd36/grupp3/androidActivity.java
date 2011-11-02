7d2570278af2360147cd4820a94534dad1a2cdcb
David Buö <davidbauwie@gmail.com>
2011-11-02 16:59:09 +0100

    okaekoawd

--- projekt.android/src/tddd36/grupp3/androidActivity.java
+++ projekt.android/src/tddd36/grupp3/androidActivity.java
@@ -18,28 +18,45 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class androidActivity extends Activity {
-	private static final String COM_IP = "130.236.227.179";
+	private static final String COM_IP = "130.236.226.95";
 	private static final int COM_PORT = 4444;
-	private static Socket client;
-	private static InputStreamReader isr;
-	private static PrintWriter pw;
-	private static BufferedReader br;
-	private static TextView display;
-	private static EditText user;
-	private static EditText pass;
-	private static String serverOutput;
-	
+	Socket client;
+	InputStreamReader isr;
+	PrintWriter pw;
+	BufferedReader br;
+	TextView display;
+	EditText user;
+	EditText pass;
+	String serverOutput;
+	Button login;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 
-		Button login;
 		pass = (EditText) findViewById(R.id.editText2);
 		user = (EditText) findViewById(R.id.editText1);
 		login = (Button) findViewById(R.id.button1);
 		display = (TextView)findViewById(R.id.textView3);
+		
+		try {
+			
+			client = new Socket(COM_IP,COM_PORT);
+			isr = new InputStreamReader(client.getInputStream());
+			pw = new PrintWriter(client.getOutputStream(),true);
+			br = new BufferedReader(isr);
+
+			
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 
 		login.setOnClickListener(new View.OnClickListener() {
 
@@ -55,40 +72,34 @@ public class androidActivity extends Activity {
 					login.show();
 				}
 
-					try {
-						login();
-					} catch (UnknownHostException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+				try {
+					login();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			};
 		});
 	}
 
-	public static void login() throws UnknownHostException, IOException {
+	public void login() throws UnknownHostException, IOException {
 		
-		client = new Socket(COM_IP,COM_PORT);
-		isr = new InputStreamReader(client.getInputStream());
-		pw = new PrintWriter(client.getOutputStream(),true);
-		br = new BufferedReader(isr);
-
-		display.setText("Försöker logga in @ "+COM_IP+":"+COM_PORT);
 		String userName = ""+user.getText();
 		String passWord = ""+pass.getText();
-		
-		pw.println(userName);
-		pw.println(passWord);
+
+		pw.println("david");
+		pw.println("davidbuo");
 
 		if((serverOutput = br.readLine()) != ""){
 			System.out.println("Server: "+serverOutput);
 			if(!serverOutput.equals("Authenticated")) System.exit(0);
 		}
-		
-	//	Intent openMenu = new Intent("tddd36.grupp3.MENU");
-		//startActivity(openMenu);
+
+		Intent openMenu = new Intent("tddd36.grupp3.MENU");
+		startActivity(openMenu);
 	}
 
 	@Override
@@ -96,6 +107,5 @@ public class androidActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onPause();
 		finish();
-
 	}
 }
