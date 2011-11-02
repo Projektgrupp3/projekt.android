67fd28433b8d25ac77663a90adedcb97f2a82add
Sanna Tyni <tyni.sanna@gmail.com>
2011-11-02 09:52:18 +0100

    ssss

--- projekt.android/src/tddd36/grupp3/androidActivity.java
+++ projekt.android/src/tddd36/grupp3/androidActivity.java
@@ -4,6 +4,7 @@ import tddd36.grupp3.R;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
